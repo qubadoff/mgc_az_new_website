90de1b58726ba495dd4c4c41f0f94b41ef43f34a
qubadoff <admin@burncode.org>
2024-12-30 22:18:12 +0400

    Service

--- resources/views/Frontend/layouts/includes/header.blade.php
+++ resources/views/Frontend/layouts/includes/header.blade.php
@@ -3,8 +3,8 @@
     <div class="uc-offcanvas-bar bg-white text-dark">
         <header class="uc-offcanvas-header hstack justify-between items-center pb-4 bg-white">
             <div class="panel uc-logo" style="top: -14px">
-                <a class='h5 text-none text-primary' href='index.html'>
-                    <img class="w-100px" src="{{ url('/') }}assets/images/common/logo-light.svg" alt="Innovar">
+                <a class='h5 text-none text-primary' href='{{ route("index") }}'>
+                    <img class="w-100px" src="{{ url('/') }}/storage/{{ siteInfo()->site_logo }}" alt="{{ siteInfo()->site_name }}">
                 </a>
             </div>
             <button class="uc-offcanvas-close p-0 icon-3 btn border-0 hover:text-primary hover:rotate-90 duration-150 transition-all" type="button">
@@ -14,100 +14,11 @@
 
         <div class="panel">
             <ul class="nav-y gap-narrow fw-bold fs-6 ft-tertiary" data-uc-nav>
-                <li class="uc-parent">
-                    <a data-uc-cursor='md bg-white opacity-10' href='index.html'>Home</a>
-                    <ul class="uc-nav-sub">
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='index.html'>Home 01</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='index-2.html'>Home 02</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='index-3.html'>Home 03</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='index-4.html'>Home 04</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='index-5.html'>Home 05</a></li>
-                    </ul>
-                </li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-services.html'>Services</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='single-service.html'>Single Service</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-case-study.html'>Case Studies</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='single-case-study.html'>Single:Case Studies</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-about.html'>About</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-blog.html'>Blog</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-pricing.html'>Pricing</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-contact.html'>Contact</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-contact-2.html'>Contact - v2</a></li>
-                <li class="uc-parent">
-                    <a href="#">Inner Pages</a>
-                    <ul class="uc-nav-sub" data-uc-nav="">
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='page-services.html'>Services</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='single-service.html'>Single Service</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='page-case-study.html'>Case Studies</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='single-case-study.html'>Single:Case Studies</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='page-about.html'>About</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='page-pricing.html'>Pricing</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='page-contact.html'>Contact</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='page-contact-2.html'>Contact - v2</a></li>
-                        <li class="uc-parent">
-                            <a data-uc-cursor='md bg-white opacity-10' href='page-blog.html'>Blog</a>
-                            <ul class="uc-nav-sub">
-                                <li><a href="blog.html" data-uc-cursor="md bg-white opacity-10">Full Width</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='blog-2cols.html'>Grid 2 Cols</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='blog-3cols.html'>Grid 3 Cols</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='blog-4cols.html'>Grid 4 Cols</a></li>
-                            </ul>
-                        </li>
-                        <li class="uc-parent">
-                            <a data-uc-cursor='md bg-white opacity-10' href='blog-detail.html'>Blog - detail</a>
-                            <ul class="uc-nav-sub">
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='blog-detail.html'>Blog detail</a></li>
-                                <li><a href="blog-detail-2.html" data-uc-cursor="md bg-white opacity-10">Blog detail - v2</a></li>
-                                <li><a href="blog-detail-3.html" data-uc-cursor="md bg-white opacity-10">Blog detail - v3</a></li>
-                            </ul>
-                        </li>
-                        <li class="uc-parent">
-                            <a href="#">Useful pages</a>
-                            <ul class="uc-nav-sub">
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='sign-up.html'>Sign up</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='sign-in.html'>Sign in</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='reset-password.html'>Reset password</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='404.html'>404 page</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='coming-soon.html'>Coming soon</a></li>
-                            </ul>
-                        </li>
-                        <li class="uc-parent">
-                            <a href="#">Other pages</a>
-                            <ul class="uc-nav-sub">
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='page-faq.html'>Faq</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='page-terms.html'>Terms of use</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='page-privacy.html'>Privacy policy</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li class="uc-parent">
-                    <a href='shop.html'>Shop</a>
-                    <ul class="uc-nav-sub" data-uc-nav="">
-                        <li class="uc-parent">
-                            <a data-uc-cursor='md bg-white opacity-10' href='shop.html'>Shop layouts</a>
-                            <ul class="uc-nav-sub">
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='shop.html'>Grid 4 cols</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='shop-3.html'>Shop 3 cols</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='shop-2.html'>Shop 2 cols</a></li>
-                                <li><a data-uc-cursor='md bg-white opacity-10' href='shop-sidebar.html'>Shop with sidebar</a></li>
-                            </ul>
-                        </li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='shop-category.html'>Archive category</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='shop-product-detail.html'>Product detail</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='shop-product-detail-2.html'>Product detail - v2</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='shop-cart.html'>Cart</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='shop-cart-2.html'>Cart - v2</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='shop-checkout.html'>Checkout</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='shop-checkout-2.html'>Checkout - v2</a></li>
-                        <li><a data-uc-cursor='md bg-white opacity-10' href='shop-order.html'>Order confirmation</a></li>
-                    </ul>
-                </li>
-                <li class="hr opacity-10 my-1"></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='sign-in.html'>Sign in</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='sign-up.html'>Sign up</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-terms.html'>Terms of use</a></li>
-                <li><a data-uc-cursor='md bg-white opacity-10' href='page-privacy.html'>Privacy policy</a></li>
+                <li><a data-uc-cursor='md bg-white opacity-10' href='{{ route("index") }}'>Home</a></li>
+                <li><a data-uc-cursor='md bg-white opacity-10' href='{{ route("about") }}'>About Us</a></li>
+                <li><a data-uc-cursor='md bg-white opacity-10' href='{{ route("faq") }}'>FAQ</a></li>
+                <li><a data-uc-cursor='md bg-white opacity-10' href='{{ route("clients") }}'>Our Clients</a></li>
+                <li><a data-uc-cursor='md bg-white opacity-10' href='{{ route("contact") }}'>Contact Us</a></li>
             </ul>
             <ul class="social-icons nav-x mt-4">
                 <li>
